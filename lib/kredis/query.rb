8ed285a4d5ada002acbf2042149613f0e7f9d5dc
Loran Kloeze <loran@codedivision.nl>
2022-06-05 16:24:08 +0200

    initial commit

---
+++ lib/kredis/query.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "query/version"
+
+module Kredis
+  module Query
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/kredis/query/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Kredis
+  module Query
+    VERSION = "0.1.0"
+  end
+end
