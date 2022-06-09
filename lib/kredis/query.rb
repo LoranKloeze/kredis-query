333a96d788bdaab90cdcbb66538acb31f54df195
Loran Kloeze <loran@codedivision.nl>
2022-06-09 09:13:26 +0200

    Implement search

--- lib/kredis/query.rb
+++ lib/kredis/query.rb
@@ -4,6 +4,7 @@ require_relative "query/version"
 require_relative "query/search"
 
 module Kredis
+  extend Query::Search
   module Query
     class Error < StandardError; end
     # Your code goes here...
--- lib/kredis/query/search.rb
+++ lib/kredis/query/search.rb
@@ -2,6 +2,16 @@
 
 module Kredis
   module Query
-    def search(a, b, c); end
+    module Search
+      def search(key_pattern, batch_size: 1000, config: :shared)
+        pattern = namespace ? "#{namespace}:#{key_pattern}" : key_pattern
+        cursor = "0"
+        loop do
+          cursor, keys = redis(config: config).scan(cursor, match: pattern, count: batch_size)
+          redis.multi { |pipeline| yield keys, pipeline }
+          break if cursor == "0"
+        end
+      end
+    end
   end
 end
